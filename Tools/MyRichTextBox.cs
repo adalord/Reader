a7ece6e4fc201db70ece444f57f0f67dbf1a8991
kovan <aeyou13@163.com>
2018-09-04 23:43:11 +0800

    只读模式，可以屏蔽光标，但是无法解决在透明模式下无法选中的问题，先还原一下

--- Tools/MyRichTextBox.cs
+++ Tools/MyRichTextBox.cs
@@ -14,14 +14,15 @@ namespace Tools
             this.GetType().GetProperty("DoubleBuffered", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).SetValue(this, true, null);
             this.Cursor = Cursors.Arrow;//设置鼠标样式  
         }
-        protected override void WndProc(ref Message m)
-        {
-            if (m.Msg == 0x7 || m.Msg == 0x201 || m.Msg == 0x202 || m.Msg == 0x203 || m.Msg == 0x204 || m.Msg == 0x205 || m.Msg == 0x206 || m.Msg == 0x0100 || m.Msg == 0x0101)
-            {
-                return;
-            }
-            base.WndProc(ref m);
-        }
+        //只读模式，可以屏蔽光标，但是无法解决在透明模式下无法选中的问题
+        //protected override void WndProc(ref Message m)
+        //{
+        //    if (m.Msg == 0x7 || m.Msg == 0x201 || m.Msg == 0x202 || m.Msg == 0x203 || m.Msg == 0x204 || m.Msg == 0x205 || m.Msg == 0x206 || m.Msg == 0x0100 || m.Msg == 0x0101)
+        //    {
+        //        return;
+        //    }
+        //    base.WndProc(ref m);
+        //}
 
     }
 }
